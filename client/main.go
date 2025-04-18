8e18ffe202e5a84c63ea7e809729f76cec382aee
Xinyi Chen <xinyi.chen@temporal.io>
2025-04-18 13:54:32 -0700

    client and server

---
+++ client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	greeting "nexus-exp/gen/proto/v1"
+	"nexus-exp/gen/proto/v1/greetingnexus"
+
+	"github.com/nexus-rpc/sdk-go/nexus"
+)
+
+func main() {
+	client, err := greetingnexus.NewGreetingNexusHTTPClient(nexus.HTTPClientOptions{
+		BaseURL: "http://localhost:7243",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	result, err := client.Greet(ctx, &greeting.GreetInput{
+		Name: "World",
+	}, nexus.ExecuteOperationOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Received greeting: %s", result.Greeting)
+}
---
+++ server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+
+	greeting "nexus-exp/gen/proto/v1"
+	"nexus-exp/gen/proto/v1/greetingnexus"
+
+	"github.com/nexus-rpc/sdk-go/nexus"
+)
+
+type handler struct {
+	greetingnexus.GreetingNexusHandler
+}
+
+func (h *handler) Greet(name string) nexus.Operation[*greeting.GreetInput, *greeting.GreetOutput] {
+	return nexus.NewSyncOperation(name, func(ctx context.Context, input *greeting.GreetInput, options nexus.StartOperationOptions) (*greeting.GreetOutput, error) {
+		return &greeting.GreetOutput{
+			Greeting: "Hello, " + input.Name,
+		}, nil
+	})
+}
+
+func main() {
+	service, err := greetingnexus.NewGreetingNexusService(&handler{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	registry := nexus.NewServiceRegistry()
+	if err := registry.Register(service); err != nil {
+		log.Fatal(err)
+	}
+	rh, err := registry.NewHandler()
+	if err != nil {
+		log.Fatal(err)
+	}
+	h := nexus.NewHTTPHandler(nexus.HandlerOptions{
+		Handler:    rh,
+		Serializer: nexus.DefaultSerializer(),
+	})
+
+	listener, err := net.Listen("tcp", "localhost:7243")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	if err = http.Serve(listener, h); err != nil {
+		log.Fatal(err)
+	}
+}
